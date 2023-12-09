3193b3c9f58367f5e8e74388fe67497934129178
TheLegendary1248 <1248alex@gmail.com>
2023-12-08 21:38:11 -0500

    More stuff

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,101 +1,136 @@
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Bounds = Matter.Bounds,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite,
-    Body = Matter.Body,
-    Vector = Matter.Vector;
+
 /**
  * The base for all Stardawn objects
  */
 class SDObject {
     
 }
-
+class SDSpace extends Pts.CanvasSpace
+{
+    /**
+     * @type {Pts.CanvasSpace()}
+     */
+    translate(x, y){
+        return this._ctx.translate(x,y)
+    }
+}
+class GameView {
+    constructor(center, minPx) {
+        
+    }
+    
+}
 // create an engine
 var engine = Engine.create();
 engine.gravity.scale = 0 
 // create a renderer
 /** @type {HTMLCanvasElement} */
 let canvasElement =  document.getElementById("canvas-renderer") 
+let ptsSpace = new SDSpace('#canvas-renderer', DoPTSThing)
 let render = Render.create({
     canvas: canvasElement,
     engine: engine,
     options: {
         background: '#fff',
-        hasBounds: true
+        hasBounds: false
     }
 });
+let center = new Pts.Pt(0,0)
+let followCenter = new Pts.Pt()
+let cursor = new Pts.Pt(0,0)
 console.log(canvasElement)
-// create pts space
-let ptsSpace = new Pts.CanvasSpace('#canvas-renderer',
-DoPTSThing
-)
-
-let ptsForm = ptsSpace.getForm()
+let runTicks = 100
+function RunPhysics() {
+    if(runTicks > 0) {
+        Engine.update(engine, 1000/60)
+        console.log("Running Physics")
+        runTicks--
+    }   
+}
+let ptsForm = new Pts.CanvasForm(ptsSpace)
+ptsSpace.background = '#0001'
 function DoPTSThing() {
 console.log(ptsForm)
 console.log(ptsForm.ctx)
 ptsForm.arc(new Pts.Pt([30,30]), 40, 79, 189, false)
-ptsSpace.add( (time, ftime) => {
-    Render.world(render, time)
-    ptsForm.stroke("#fff").fill("#f03").circle( [10, 10] );
-    ptsForm.point( [25, 47], 10 );
+ptsSpace.autoResize = true
+ptsSpace.add( 
+    { animate: (time, ftime) => {
+    RunPhysics()
+    let m = ptsSpace.ctx.globalCompositeOperation
+    ptsSpace.ctx.globalCompositeOperation = "source-in"
+    ptsSpace.clear()
+    ptsForm.composite()
+    ptsSpace.translate(ptsSpace.width/2, ptsSpace.height/2)
+    followCenter = Pts.Geom.interpolate(followCenter, center, Math.min(ftime * 6 / 1000, 1))
+    ptsSpace.translate(followCenter.x, followCenter.y)
+    //Cursor
+    ptsForm.fillOnly("#112").circle(Pts.Circle.fromCenter([0,0], 516));
+    ptsForm.strokeOnly("#fff", 3).dash(true,time/200).point(cursor, 16, "circle")
+    ptsForm.fillOnly("#f00").point(cursor, 3, "circle")
+    //Reset
+    ptsForm.dash(false).fill(false).stroke("#f00",4)
+    ptsSpace.ctx.scale(1,1)
     
-    //console.log(time)
-} );
-ptsSpace.play()
+    CustomWorldRender(render, time)
+}, action: (type, px, py, evt) => 
+{ 
+    inlog(evt)
+    if (evt.type )
+    if (type == "keydown") 
+    {
+        var moveFunc = (m) => { center.add(m); console.log(m) } 
+        switch (evt.key) {
+            case "w": case "ArrowUp": moveFunc([0,10]); break;
+            case "a": case "ArrowLeft": moveFunc([10,0]); break;
+            case "s": case "ArrowDown": moveFunc([0,-10]); break;
+            case "d": case "ArrowRight": moveFunc([-10,0]); break;
+            case "space": 
+        }
+    }
+    if (type == "drag")
+    {
+        center.add([evt.movementX,evt.movementY])
+    }
+    if (type == "click") {cursor = (new Pts.Pt([px - ptsSpace.width/2, py - ptsSpace.height/2])).$subtract(center)}
+    console.log(type, px, py, evt)
 }
+} );
+ptsSpace.bindMouse().bindTouch().bindKeyboard().play()
+}
+
 function RunFrame(){
     Engine.update(engine)
-    console.log("Ran Matter Frame")
+    //console.log("Ran Matter Frame")
 }
-var CreatePlayerBody = (x,y) => Bodies.circle(x,y,20,{frictionAir: 0})
 
-var PlayerA = CreatePlayerBody(40,80)
-var PlayerB = CreatePlayerBody(80,40)
+var CreatePlayerBody = (x,y) => Bodies.fromVertices(x,y, Matter.Vertices.create([{x:0,y:30},{x:15, y:-15},{x:-15, y: -15}]),{render:{fillStyle:'#f00'}})
 
+var PlayerA = CreatePlayerBody(-200,-200)
+var PlayerB = CreatePlayerBody(200,200)
+var asteroids = []
 // create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
+for(var i = 0; i < 40; i++) asteroids.push(Bodies.circle(Math.random() * 1000 - 500, Math.random() * 1000 - 500, Math.random() * 40 + 10));
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground, PlayerA, PlayerB]);
+Composite.add(engine.world, [...asteroids, PlayerA, PlayerB]);
 
-// run the renderer
-//Render.run(render);
-function addForces() {
-    Body.applyForce(PlayerA, PlayerA.position, Vector.create(.01,.01))
-    Body.applyForce(PlayerB, PlayerB.position, Vector.create(-1.0,-1.0))
-}
-window.addEventListener('mousedown',()=>addForces())
-window.addEventListener('keydown',RunFrame)
-/**
- * Poor function that does the thing it needs to 
- * @param {Matter.Render} render 
- */
-function DebugRender(render) {
 
-}
 //This broke introducing PTS. Will fix later
-// function fitCanvas() {
-//     //https://github.com/liabru/matter-js/issues/955
-//     var winHeight = window.innerHeight;
-//     var winWidth = window.innerWidth;
-//     render.bounds.max.y = 800 * (Math.max(winWidth, winHeight) / winWidth); 
-//     render.bounds.max.x = 800 * (Math.max(winWidth, winHeight) / winHeight);
-//     render.bounds.min = Vector.create(0,0);
-//     Bounds.shift(render.bounds, Vector.create(0,0))
-//     console.log(`${winWidth}/${winHeight}`)
-//     render.options.width = winWidth;
-//     render.options.height = winHeight;
-//     render.canvas.width = winWidth;
-//     render.canvas.height = winHeight;
-// }
-// fitCanvas()
+function fitCanvas() {
+    //https://github.com/liabru/matter-js/issues/955
+    var winHeight = window.innerHeight;
+    var winWidth = window.innerWidth;
+    render.bounds.max.y = 800 * (Math.max(winWidth, winHeight) / winWidth); 
+    render.bounds.max.x = 800 * (Math.max(winWidth, winHeight) / winHeight);
+    render.bounds.min = Vector.create(0,0);
+    Bounds.shift(render.bounds, Vector.create(0,0))
+    console.log(`${winWidth}/${winHeight}`)
+    render.options.width = winWidth;
+    render.options.height = winHeight;
+    render.canvas.width = winWidth;
+    render.canvas.height = winHeight;
+}
+fitCanvas()
 //window.addEventListener('resize', fitCanvas);
-
 //little experimental thing
 //fetch("/components/card.html").then((r) => r.text().then((html) => document.getElementById("card-container").innerHTML = html.repeat(4))) 
-//export default render 
---
+++ frontend/src/mangle.js
@@ -0,0 +1,138 @@
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Bounds = Matter.Bounds,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite,
+    Common = Matter.Common,
+    Events = Matter.Events,
+    Body = Matter.Body,
+    Vector = Matter.Vector;
+var inlogElement = document.getElementById('inlog')
+function inlog(){inlogElement.innerText = Array.from(arguments).map(JSON.stringify)} 
+var CustomWorldRender = function(render, time) {
+        var startTime = Common.now(),
+            engine = render.engine,
+            world = engine.world,
+            canvas = render.canvas,
+            context = render.context,
+            options = render.options,
+            timing = render.timing;
+
+        var allBodies = Composite.allBodies(world),
+            allConstraints = Composite.allConstraints(world),
+            background = options.wireframes ? options.wireframeBackground : options.background,
+            bodies = [],
+            constraints = [],
+            i;
+
+        var event = {
+            timestamp: engine.timing.timestamp
+        };
+
+        Events.trigger(render, 'beforeRender', event);
+
+        // Background application here is removed
+        // clear the canvas with a transparent fill, to allow the canvas background to show
+        // context.globalCompositeOperation = 'source-in';
+        // context.fillStyle = "transparent";
+        // context.fillRect(0, 0, canvas.width, canvas.height);
+        context.globalCompositeOperation = 'source-over';
+
+        // handle bounds
+        if (options.hasBounds) {
+            // filter out bodies that are not in view
+            for (i = 0; i < allBodies.length; i++) {
+                var body = allBodies[i];
+                if (Bounds.overlaps(body.bounds, render.bounds))
+                    bodies.push(body);
+            }
+
+            // filter out constraints that are not in view
+            for (i = 0; i < allConstraints.length; i++) {
+                var constraint = allConstraints[i],
+                    bodyA = constraint.bodyA,
+                    bodyB = constraint.bodyB,
+                    pointAWorld = constraint.pointA,
+                    pointBWorld = constraint.pointB;
+
+                if (bodyA) pointAWorld = Vector.add(bodyA.position, constraint.pointA);
+                if (bodyB) pointBWorld = Vector.add(bodyB.position, constraint.pointB);
+
+                if (!pointAWorld || !pointBWorld)
+                    continue;
+
+                if (Bounds.contains(render.bounds, pointAWorld) || Bounds.contains(render.bounds, pointBWorld))
+                    constraints.push(constraint);
+            }
+
+            // transform the view
+            //Render.startViewTransform(render);
+
+            // update mouse
+            if (render.mouse) {
+                Mouse.setScale(render.mouse, {
+                    x: (render.bounds.max.x - render.bounds.min.x) / render.options.width,
+                    y: (render.bounds.max.y - render.bounds.min.y) / render.options.height
+                });
+
+                Mouse.setOffset(render.mouse, render.bounds.min);
+            }
+        } else {
+            constraints = allConstraints;
+            bodies = allBodies;
+
+            if (render.options.pixelRatio !== 1) {
+                render.context.setTransform(render.options.pixelRatio, 0, 0, render.options.pixelRatio, 0, 0);
+            }
+        }
+
+        if (!options.wireframes || (engine.enableSleeping && options.showSleeping)) {
+            // fully featured rendering of bodies
+            Render.bodies(render, bodies, context);
+        } else {
+            if (options.showConvexHulls)
+                Render.bodyConvexHulls(render, bodies, context);
+
+            // optimised method for wireframes only
+            Render.bodyWireframes(render, bodies, context);
+        }
+
+        if (options.showBounds)
+            Render.bodyBounds(render, bodies, context);
+
+        if (options.showAxes || options.showAngleIndicator)
+            Render.bodyAxes(render, bodies, context);
+
+        if (options.showPositions)
+            Render.bodyPositions(render, bodies, context);
+
+        if (options.showVelocity)
+            Render.bodyVelocity(render, bodies, context);
+
+        if (options.showIds)
+            Render.bodyIds(render, bodies, context);
+
+        if (options.showSeparations)
+            Render.separations(render, engine.pairs.list, context);
+
+        if (options.showCollisions)
+            Render.collisions(render, engine.pairs.list, context);
+
+        if (options.showVertexNumbers)
+            Render.vertexNumbers(render, bodies, context);
+
+        if (options.showMousePosition)
+            Render.mousePosition(render, render.mouse, context);
+
+        Render.constraints(constraints, context);
+
+        // if (options.hasBounds) {
+        //     // revert view transforms
+        //     Render.endViewTransform(render);
+        // }
+
+        Events.trigger(render, 'afterRender', event);
+
+        // log the time elapsed computing this update
+        timing.lastElapsed = Common.now() - startTime;
+    };
